33276e531407ea347c5ac8dc6ce269879240b533
Vaibhav Raj <vaibhav.work.code@gmail.com>
2025-08-17 01:57:57 +0530

    added new stl folder

---
+++ STL/pairs_in_cpp.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // pairs in cpp 
+    pair <int, int> p1 = {1, 2};
+    cout << p1.first << " and " << p1.second << endl;
+
+    return 0;
+}
