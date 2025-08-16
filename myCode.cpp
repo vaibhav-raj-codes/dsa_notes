f12e415cdfce3494f7d99ba38dc42e98ccaee9f2
= <=>
2025-08-16 17:34:24 +0530

    first commit

---
+++ myCode.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cout << "hello, what is your name?" << endl;
+    cin >> name;
+    cout << "The name inputed is " << name << endl;
+    return 0;
+}
